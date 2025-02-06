12cf859adced7770aa06694beb9110212c1e01db
Vladislav Sadovin <sadovin.vladislav@gmail.com>
2025-02-06 21:33:53 +0300

    Create migration for sources table

---
+++ database/migrations/2025_02_06_182252_create_sources_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sources', function (Blueprint $table) {
+            $table->id();
+            $table->string('url', 128)->nullable(false);
+            $table->string('parser', 64)->nullable(false);
+            $table->boolean('is_active')->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sources');
+    }
+};
