e53f1de87fab1b8442a0874eae78db5b5ebe2a81
Vladislav Sadovin <sadovin.vladislav@gmail.com>
2025-02-16 21:27:25 +0300

    Updated sources info

--- app/Helpers/SourceHelper.php
+++ app/Helpers/SourceHelper.php
@@ -7,16 +7,18 @@ use App\Models\Source;
 class SourceHelper
 {
     const SOURCE_EXIST_MESSAGE = 'Source exist';
+    const PARSERS_NAMESPACE = 'App\Parsers\\';
     public static function addNewSource(string $url): bool|string
     {
         $parser = match (parse_url($url)['host']) {
-            HostsParsers::VKParser->value => HostsParsers::VKParser->name,
+            HostsParsers::VKParser->value => self::PARSERS_NAMESPACE . HostsParsers::VKParser->name,
             default => 'DefaultParser'
         };
 
         if (!is_null(Source::where('url', $url)->first())) {
             return self::SOURCE_EXIST_MESSAGE;
         }
+
 
         Source::create([
             'url' => $url,
@@ -30,7 +32,7 @@ class SourceHelper
     public static function parseSource(Source $source, string $keyUrl): void {
         echo 'Parsing ' . $keyUrl . '...' . PHP_EOL;
 
-        $parserName = 'App\Parsers\\' . $source['parser'];
+        $parserName = $source['parser'];
         $parser = new $parserName($keyUrl);
         $parser->run();
     }
--- app/Parsers/VKParser.php
+++ app/Parsers/VKParser.php
@@ -2,45 +2,66 @@
 
 namespace App\Parsers;
 
+use AllowDynamicProperties;
 use App\Models\Source;
 use App\Parsers\AbstractParser;
 use VK\Client\VKApiClient;
 
-class VKParser extends AbstractParser
+#[AllowDynamicProperties] class VKParser extends AbstractParser
 {
     private array $posts;
     private Source $source;
     public function __construct(string $url)
     {
         //TODO: если будет много манипуляций с урлами, вынести в отдельный хелпер
+        $this->vkApi = new VKApiClient();
         $this->url = ltrim(parse_url($url)['path'], '/');
-        $this->source = Source::where('url', $url)->first();
         $this->posts = [];
+        $this->source = Source::where('url', $url)->first();
+        if (is_array($this->source['extra']) && !array_key_exists('groupId', $this->source['extra']) || !$this->source['extra']) {
+            $this->source['extra'] = ['groupId' => -$this->getGroupId()];
+            $this->source->save();
+        }
     }
 
     protected function getContent(): void
     {
-        $vk = new VKApiClient();
-
-        $response = $vk->wall()->get(env('VK_API_ACCESS_TOKEN'), [
-            'domain' => $this->url,
+        $response = $this->vkApi->wall()->get(env('VK_API_ACCESS_TOKEN'), [
+            'owner_id' => $this->source['extra']['groupId'],
             'count' => 10
         ]);
 
-        if (!$this->source['extra'] || $this->source['extra']['currentCount'] < $response['count']) {
-            $this->posts = is_array($this->source['extra']) && array_key_exists('currentCount', $this->source['extra'])
+        if (array_key_exists('currentCount', $this->source['extra'])) {
+            $this->posts = ($this->source['extra']['currentCount'] < $response['count'])
                 ? array_slice($response['items'], 0, $response['count'] - $this->source['extra']['currentCount'] + 1)
-                : $response['items'];
-            $this->source['extra'] = [
-                'currentCount' => $response['count']
-            ];
-            $this->source->save();
+                : [];
+        } else {
+            $this->posts = $response['items'];
         }
+
+        $extra = [
+            'currentCount' => $response['count'],
+        ];
+
+        $this->source['extra'] = array_merge($this->source['extra'], $extra);
+        $this->source->save();
+        dd($this->posts);
     }
 
     protected function parseContent()
     {
         // TODO: Implement parseContent() method.
         echo 'parse content in ' . $this->url . PHP_EOL;
+    }
+
+    private function getGroupId(): int {
+        $response = $this->vkApi
+            ->utils()
+            ->resolveScreenName(env('VK_API_ACCESS_TOKEN'), [
+                    'screen_name' => $this->url
+                ]
+            );
+
+        return $response['object_id'];
     }
 }
