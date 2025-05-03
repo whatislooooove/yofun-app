8b8c6ab6076876f7d934c2782f99b0cc22d81c54
Vladislav Sadovin <sadovin.vladislav@gmail.com>
2025-05-03 13:54:06 +0300

    Frontend features

--- resources/views/components/card-table.blade.php
+++ resources/views/components/card-table.blade.php
@@ -9,15 +9,13 @@
                 <div class="card d-flex flex-column h-100">
                     <img src="{{ $item->img ?? asset('/storage/img/default.jpg') }}" class="card-img-top" alt="...">
                     <div class="card-body d-flex flex-column justify-content-between">
-                        <h5 class="card-title">{{ $item->title }}</h5>
-                        <button type="button"
-                                class="btn btn-secondary"
-                                data-bs-container="body"
-                                data-bs-toggle="popover"
-                                data-bs-placement="bottom"
-                                data-bs-content="{{ $item->description }}">
+                        <h5 class="card-title text-white">{{ $item->title }}</h5>
+                        <a href="{{ $item->detail_url }}"
+                            type="button"
+                            class="btn btn-secondary"
+                            target="_blank">
                             Подробнее
-                        </button>
+                        </a>
                     </div>
                 </div>
             </div>
