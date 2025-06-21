e799b76b505c6c5bd29921aace57b36f934a505a
Vladislav Sadovin <sadovin.vladislav@gmail.com>
2025-06-22 00:27:40 +0300

    Added swagger

--- src/backend/app/Http/Controllers/API/V1/MainController.php
+++ src/backend/app/Http/Controllers/API/V1/MainController.php
@@ -6,10 +6,43 @@ use App\DTO\AnnouncementsDTO;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\AnnouncementResource;
 use App\Models\Announcement;
+use OpenApi\Attributes as OA;
 
+#[OA\Info(version: "1.0.0", title: "yofun API Doc")]
 class MainController extends Controller
 {
     const int ITEMS_PER_PAGE = 12;
+
+    #[OA\Get(
+        path: '/api/v1',
+        summary: 'Get data for index page',
+        tags: ['main'],
+        responses: [
+            new OA\Response(
+                response: 200,
+                description: 'OK',
+                content: new OA\JsonContent(
+                    properties: [
+                        new OA\Property(
+                            property: 'sliderEvents',
+                            type: 'array',
+                            items: new OA\Items(ref: '#/components/schemas/AnnouncementResource')
+                        ),
+                        new OA\Property(
+                            property: 'upcomingEvents',
+                            type: 'array',
+                            items: new OA\Items(ref: '#/components/schemas/AnnouncementResource')
+                        ),
+                        new OA\Property(
+                            property: 'upcomingQuizzes',
+                            type: 'array',
+                            items: new OA\Items(ref: '#/components/schemas/AnnouncementResource')
+                        ),
+                    ]
+                )
+            )
+        ]
+    )]
     public function index() {
         $eventsData = AnnouncementsDTO::fromCollection(Announcement::active()->get());
 
@@ -20,10 +53,78 @@ class MainController extends Controller
         ]);
     }
 
+    #[OA\Get(
+        path: '/api/v1/quizzes',
+        summary: 'Get all quizzes',
+        tags: ['main'],
+        parameters: [
+            new OA\Parameter(
+                name: 'page',
+                description: 'Get items in target page',
+                in: 'query',
+                required: false,
+                schema: new OA\Schema(type: 'number')
+            )
+        ],
+        responses: [
+            new OA\Response(
+                response: 200,
+                description: 'OK',
+                content: new OA\JsonContent(
+                    properties: [
+                        new OA\Property(
+                            property: 'data',
+                            type: 'array',
+                            items: new OA\Items(ref: '#/components/schemas/AnnouncementResource')
+                        ),
+                        new OA\Property(
+                            property: 'meta',
+                            type: 'array',
+                            items: new OA\Items()
+                        )
+                    ]
+                )
+            )
+        ]
+    )]
     public function quizzes() {
         return AnnouncementResource::collection(Announcement::active()->quizzes()->paginate(self::ITEMS_PER_PAGE));
     }
 
+    #[OA\Get(
+        path: '/api/v1/events',
+        summary: 'Get all events',
+        tags: ['main'],
+        parameters: [
+            new OA\Parameter(
+                name: 'page',
+                description: 'Get items in target page',
+                in: 'query',
+                required: false,
+                schema: new OA\Schema(type: 'number')
+            )
+        ],
+        responses: [
+            new OA\Response(
+                response: 200,
+                description: 'OK',
+                content: new OA\JsonContent(
+                    properties: [
+                        new OA\Property(
+                            property: 'data',
+                            type: 'array',
+                            items: new OA\Items(ref: '#/components/schemas/AnnouncementResource')
+                        ),
+                        new OA\Property(
+                            property: 'meta',
+                            type: 'array',
+                            items: new OA\Items()
+                        )
+                    ]
+                )
+            )
+        ]
+    )]
     public function events() {
         return AnnouncementResource::collection(Announcement::active()->events()->paginate(self::ITEMS_PER_PAGE));
     }
--- src/backend/app/Http/Resources/AnnouncementResource.php
+++ src/backend/app/Http/Resources/AnnouncementResource.php
@@ -5,7 +5,24 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Carbon;
+use OpenApi\Attributes as OA;
 
+#[OA\Schema(
+    schema: 'AnnouncementResource',
+    properties: [
+        new OA\Property(property: 'id', type: 'number', example: '1'),
+        new OA\Property(property: 'title', type: 'string', example: 'Трибьютный концерт'),
+        new OA\Property(property: 'description', type: 'string', example: 'Описание трибьютного концерта'),
+        new OA\Property(property: 'detail', type: 'string', example: 'example.com'),
+        new OA\Property(property: 'date', type: 'string', example: '2025-06-21'),
+        new OA\Property(property: 'time', type: 'string', example: '20:00'),
+        new OA\Property(property: 'location', type: 'string', example: 'паб Бульдог'),
+        new OA\Property(property: 'price', type: 'number', example: '1500'),
+        new OA\Property(property: 'category', type: 'string', example: 'концерты'),
+        new OA\Property(property: 'image', type: 'string', example: 'example.com/image.png'),
+    ],
+    type: 'object'
+)]
 class AnnouncementResource extends JsonResource
 {
     /**
