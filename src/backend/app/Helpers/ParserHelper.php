2ec0e9dcabe9d9f81284932b7aa9df3ebdd5dc91
Vladislav Sadovin <sadovin.vladislav@gmail.com>
2025-08-27 21:26:25 +0300

    Fix namespace bug

--- src/backend/app/Helpers/ParserHelper.php
+++ src/backend/app/Helpers/ParserHelper.php
@@ -2,26 +2,27 @@
 
 namespace App\Helpers;
 
+use app\Crawlers\Social\VKParser;
 use App\Enums\HostsParsers;
 use Illuminate\Support\Facades\File;
 
 final class ParserHelper
 {
-    const PARSERS_NAMESPACE = 'App\Parsers\\';
     const EXCLUDED_PARSERS_FOR_CHOOSE = [
         'IParser.php',
         'AbstractParser.php',
+        'WebParser.php',
         'VKParser.php',
         'TGParser.php'
     ];
     public static function isSpecificSource(string $url): string|bool {
         return match (parse_url($url)['host']) {
-            HostsParsers::VKParser->value => self::PARSERS_NAMESPACE . HostsParsers::VKParser->name,
+            HostsParsers::VKParser->value => VKParser::class,
             default => true
         };
     }
     public static function getParsersList(): array {
-        $actualParsers = array_values(array_filter(File::allFiles('app/Parsers'), function ($item) {
+        $actualParsers = array_values(array_filter(File::allFiles('app/Crawlers'), function ($item) {
             if (!in_array($item->getRelativePathname(), self::EXCLUDED_PARSERS_FOR_CHOOSE)) {
                 return $item->getPathname();
             }
